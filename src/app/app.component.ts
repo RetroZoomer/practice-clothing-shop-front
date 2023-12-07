acde5c0544b1f03d50b09649f4c6b3d505cd7526
RetroZoomer <bayan-herov@mail.ru>
2023-12-07 13:09:48 +0300

    Компонент продукта

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {IProduct} from "./models/product";
+import {product as data} from "./data/product";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'practice-clothing-shop-front';
+  products: IProduct[] = data
 }
---
+++ src/app/data/product.ts
@@ -0,0 +1,16 @@
+import {IProduct} from "../models/product";
+
+export const product: { image: string; price: number; rating: { rate: number; count: number }; description: string; id: number; title: string; category: string }[] = [
+  {
+    id: 1,
+    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    price: 109.95,
+    description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    category: "men's clothing",
+    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    rating: {
+        rate: 3.9,
+        count: 120
+    }
+  }
+]
